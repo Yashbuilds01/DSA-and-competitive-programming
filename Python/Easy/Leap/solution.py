e3fa2e105d43045f696a15b5f66f78494e573a08
Yash <its.yash.goyal24@gmail.com>
2026-08-24 20:54:21 +0530

    Added question-4 with solution

---
+++ Python/Easy/Leap/solution.py
@@ -0,0 +1,10 @@
+def LeapYear(n):
+    if n % 4==0:
+        if n % 100 ==0 and n % 400 !=0:
+            print("NO")
+        else:
+            print("YES")
+    else:
+        print("NO")
+n= int(input())
+LeapYear(n)
